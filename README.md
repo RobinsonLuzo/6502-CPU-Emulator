090c57d3ba30b2aeb17fb2fe7353594bc0c5b1c0
RobinsonLuzo <bendanielfagan@gmail.com>
2021-03-19 14:47:46 +0000

    First commit - basic CPU funcitonality with 1 operation.

---
+++ README.md
@@ -0,0 +1,36 @@
+# 6502 CPU emulator:
+
+Work in Progress.....
+
+
+
+Emulation of a Commodore 64 like 8 bit 6502 microporcessor in C++. A contemporary of the Intel 8080, it was introduced in 1975. The chip (and variations) were used in a number of singificant machines including:
+
+- [Apple II](https://en.wikipedia.org/wiki/Apple_II)
+- [Nintendo Entertainment System (NES)](https://en.wikipedia.org/wiki/Nintendo_Entertainment_System)
+- [Commodore 64](https://en.wikipedia.org/wiki/Commodore_64)
+- [BBC Micro](https://en.wikipedia.org/wiki/BBC_Micro)
+
+## Creation:
+
+First, we need to emulate the CPU's [Registers](http://www.obelisk.me.uk/6502/registers.html). There aren't many thankfully - it's much simpler than a modern processor. These are:
+- Program Counter: Pointer to the address the CPU is executing the code for.
+- Stack Pointer: current part of the stack the program is using. For this it consists of a single 256 byte thread.
+- Accumulator: 8 bit register.
+- Index Register X: 8 bit register.
+- Index Register Y: 8 bit register.
+- Processor Status: a single byte with several flags in it that are set to different statuses depending on what's happening. These flags are set or cleared depending on the instruction executing.
+
+An inital boot sequence is made for these.
+
+Secondly, we need memory. This is straighforward enough: 1024 * 64 is the max we can have. Create a method to initialise this on startup to all 0s.
+
+For each tick of the clock we execute a CPU instruction. These [instructions](http://www.obelisk.me.uk/6502/instructions.html) are straighforward enough.
+
+
+
+This C++ project uses Visual Studio Code with GCC compiler (g++) and GDB debugger from mingw-w64. [Info](https://code.visualstudio.com/docs/cpp/config-mingw)
+
+## Sources:
+[6502 Wiki](https://en.wikipedia.org/wiki/MOS_Technology_6502)
+[6502 Architecture](http://www.obelisk.me.uk/6502/index.html)
---
+++ main_6502.cpp
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Defining 8 and 16 bit types respectively
+using Byte = unsigned char;
+using Word = unsigned short;
+
+using u32 = unsigned int;
+
+// Memory
+struct Mem {
+
+    static constexpr u32 MAX_MEM = 1024 * 64;
+    Byte Data[MAX_MEM];
+
+    void Initialise() {
+
+        for (u32 i = 0; i < MAX_MEM; i++) {
+            Data[i] = 0;
+        }
+    }
+
+    // Get/Read a Byte out of memory:
+    Byte operator[](u32 Address) const {
+        return Data[Address];
+    }
+
+    // Write 1 byte:
+    Byte& operator[](u32 Address) {
+        return Data[Address];
+    }
+
+};
+
+
+// Main CPU body
+struct CPU {
+    
+    Word PC; // Program counter
+    Word SP; // Stack Pointer
+
+    Byte A, X, Y; // Accumulator, Index X and Y
+
+    // Bitfields represent Processor Status - status flags:
+    Byte C : 1; // Carry flag
+    Byte Z : 1; // Zero flag
+    Byte I : 1; // Interrupt Disable
+    Byte D : 1; // Decimal mode
+    Byte B : 1; // Break Command
+    Byte V : 1; // Overflow flag
+    Byte N : 1; // Negative flag
+
+
+    // reset CPU as if the computer was turned on:
+    // Boot up seqeunce sets memory to a given point
+    void Reset(Mem& memory) {
+        
+        PC = 0xFFFC;
+        SP = 0x0100; // Initialised to 100
+        C = Z = I = D = B = V = N = 0;
+        A = X = Y = 0;
+        memory.Initialise();
+
+    }
+
+    // retrieve instruction byte
+    Byte FetchByte(u32& Cycles, Mem& memory) {
+        // Go to memory and take the next byte from wherever Program Counter tells us
+        Byte Data = memory[PC];
+        PC++;                   
+        Cycles--;   // Move on to next step
+        return Data;
+    }
+
+    // Opcodes:
+    static constexpr Byte
+        // Load Accumulator - immediate:
+        INS_LDA_IM = 0xA9;
+
+
+    // execute instructions for given number of clock ticks
+    void Execute(u32 Cycles, Mem& memory) {
+
+        while (Cycles > 0) {
+            // fetch next instruction from memory
+            Byte Ins = FetchByte(Cycles, memory);
+            switch (Ins) {
+                case INS_LDA_IM: {
+                    printf("Switched! %d \n", Ins);
+                    Byte value = FetchByte(Cycles, memory);
+                    A = value;                  // Set A register
+                    Z = (A == 0);               // Zeros reg set if A=0
+                    N = (A & 0b10000000) > 0;   // Negative flag set if 7th bit of A is set
+                } break;
+                default: {
+                    printf("Instruction not handled %d", Ins);
+                } break;
+            } 
+            
+        }
+
+    }
+
+};
+
+
+int main() {
+
+    Mem mem;
+    CPU cpu;
+    cpu.Reset(mem);
+    // Start - a little inline program
+    mem[0xFFFC] = CPU::INS_LDA_IM;
+    mem[0xFFFD] = 0x42;
+    // end - a little inline program
+    cpu.Execute(2, mem);
+
+    printf("Hello Everyone");
+    return 0;
+}
